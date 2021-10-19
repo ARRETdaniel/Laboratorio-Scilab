8d924710fa26a176b5fd94b3e2d0f208e88eb4e8
Daniel Terra Gomes <danielterra24@hotmail.com>
2021-10-19 18:19:00 -0300

    done

--- Laboratorio-Scilab01/matrizes.m
+++ Laboratorio-Scilab01/matrizes.m
@@ -8,14 +8,33 @@
 
 clc; clear;
 fprintf('UENF - Ciencia da Computacao\n');
-fprintf("Aluno: Fulano de tal - Campos %s ", date());
+fprintf("Aluno: Daniel Terra Gomes %s ", date());
 
 %==================
-test1 = zeros(9) % X = zeros(n) returns an n-by-n matrix of zeros.
-test2 = zeros(2,5) %  zeros(2,3) returns a 2-by-3 matrix.
+test1 = zeros(5) % X = zeros(n) returns an n-by-n matrix of zeros.
+test2 = zeros(5,5) %  zeros(2,3) returns a 2-by-3 matrix.
+
+%=================
+% 9 Criar matrizes 4x3 de números inteiros:  A, B, C e D e a partir delas, criar a matriz M = [A B C; D A B]
+
+A = 1*ones(4,3) % C = 2*ones(1,3) same as C = [2 2 2; 2 2 2]
+B = 2*ones(4,3) % C = 2*ones(1,3) same as C = [2 2 2; 2 2 2]
+C = 3*ones(4,3) % C = 2*ones(1,3) same as C = [2 2 2; 2 2 2]
+D = 4*ones(4,3) % C = 2*ones(1,3) same as C = [2 2 2; 2 2 2]
+M =  [A B C; D A B]     %// matriz
 
 %=================
 
+N = 8*rand(6,6) % C = 2*ones(1,3) same as C = [2 2 2; 2 2 2]
+fprintf("N(3,4)  %f \n", N(3,4));
+fprintf("N(:, 5)  %f \n", N(:, 5));
+fprintf(" N(3,:)  %f \n", N(3,:));
+fprintf(" N(2:4, 4:6) %f \n", N(2:4, 4:6));
+%N(:, 5)
+%N(3,:)
+%N(2:4, 4:6)
+
+%=================
 m1 = [1 2 3; 4 5 6; 7 8 9]
 
 m2 = [ 3 5; 6 9]
